f3c90e805b999844f6b7fe8e95a10c3a3e52634f
irfanalarib <irfanalarib7@gmail.com>
2026-06-20 17:02:13 +0700

    menambahkan event pada button load

--- CRUDMahasiswaADO/Dashboard.cs
+++ CRUDMahasiswaADO/Dashboard.cs
@@ -24,7 +24,8 @@ namespace CRUDMahasiswaADO
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            button = 1;
+            loadDataChart();
         }
 
         
