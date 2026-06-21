9b8046d3d93b8509712150959fd851dff2189bee
irfanalarib <irfanalarib7@gmail.com>
2026-06-21 12:59:22 +0700

    tambah navigasi ke form Data Mahasiswa

--- CRUDMahasiswaADO/Dashboard.cs
+++ CRUDMahasiswaADO/Dashboard.cs
@@ -148,5 +148,12 @@ namespace CRUDMahasiswaADO
             button = 0;
             loadDataChart();
         }
+
+        private void btnDataMhs_Click(object sender, EventArgs e)
+        {
+            Form1 frm1 = new Form1();
+            frm1.Show();
+            this.Hide();
+        }
     }
 }
