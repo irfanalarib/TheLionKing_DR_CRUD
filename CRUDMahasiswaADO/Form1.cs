a869113d4768dac6fa70d939e2875f8cce367a5c
irfanalarib <irfanalarib7@gmail.com>
2026-06-21 14:09:10 +0700

    menambahkan fungsi pada btnUploadGambar_Click

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -370,5 +370,16 @@ namespace CRUDMahasiswaADO
             this.Hide();
         }
 
+        private void btnUploadGambar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                fotoMhs.Image = Image.FromFile(ofd.FileName);
+                fotoMhs.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
     }
 }
